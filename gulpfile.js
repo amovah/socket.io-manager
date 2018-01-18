c05af5ed172723145040c97e2c5ade4955c6f4d5
Ali Movahedi <ali_movahedi@aol.com>
2018-01-18 13:27:19 +0330

    feat: add uglify to prod

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,22 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var eslint = require('gulp-eslint');
 var del = require('del');
+var uglify = require('gulp-uglify');
 
 gulp.task('clean', function() {
   return del('build');
 });
 
-gulp.task('babel', ['clean'], function() {
+gulp.task('babel:dev', ['clean'], function() {
   return gulp.src('src/**/*.js')
         .pipe(babel())
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('babel:prod', ['clean'], function() {
+  return gulp.src('src/**/*.js')
+        .pipe(babel())
+        .pipe(uglify())
         .pipe(gulp.dest('build'));
 });
 
@@ -22,9 +30,9 @@ gulp.task('lint', function() {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('prod', ['lint', 'babel']);
+gulp.task('prod', ['lint', 'babel:prod']);
 
-gulp.task('dev', ['babel']);
+gulp.task('dev', ['babel:dev']);
 
 gulp.task('watch', function() {
   gulp.watch('src/**/*.js', ['dev']);
