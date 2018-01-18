089f6ce9306de37a8de92616062d0a44b15f19d7
Ali Movahedi <ali_movahedi@aol.com>
2018-01-18 13:12:13 +0330

    initial commit :tada:

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
+var del = require('del');
+
+gulp.task('clean', function() {
+  return del('build');
+});
+
+gulp.task('babel', ['clean'], function() {
+  return gulp.src('src/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src('src/**/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('prod', ['lint', 'babel']);
+
+gulp.task('dev', ['babel']);
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.js', ['dev']);
+});
